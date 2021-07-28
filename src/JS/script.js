be9d48839a0eb4c890c4a7de8eb178b3cda91a94
Gleb <vasiliev19.06gleb@gmail.com>
2021-07-28 16:49:19 +0300

    do modal and validated

--- src/JS/script.js
+++ src/JS/script.js
@@ -63,4 +63,46 @@ $(document).ready(function(){
       };
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      //modal
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: "required",
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: "Пожалуйста, введите своё имя",
+          phone: "Пожалуйста, введите свой телефонный номер",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введён адрес почты"
+          }
+        }
+      });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask(" +7 (999) 999-99-99");
 });
